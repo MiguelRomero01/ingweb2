efdbb059b3802778a3dcc62a74718bc12d5d4f44
miguel angel romero parra <mr9501459@gmail.com>
2024-09-12 21:03:14 -0500

    navbar v:1.0

---
+++ layouts/navbar/navbarCars.js
@@ -0,0 +1,6 @@
+fetch('/layouts/navbar/navbarCars.html')
+          .then(response => response.text())
+          .then(data => {
+              document.getElementById('navBar-placeholder').innerHTML = data;
+          })
+          .catch(error => console.error('Error al cargar el navbar:', error));
