2a10ebfcc9928d3804ad70639814b128a3eebc13
miguel angel romero parra <mr9501459@gmail.com>
2024-09-15 15:58:48 -0500

    estructuracion proyecto

--- layouts/navbar/navbarCars.js
+++
@@ -1,6 +0,0 @@
-fetch('/layouts/navbar/navbarCars.html')
-          .then(response => response.text())
-          .then(data => {
-              document.getElementById('navBar-placeholder').innerHTML = data;
-          })
-          .catch(error => console.error('Error al cargar el navbar:', error));
--- mio.js
+++
@@ -1,6 +0,0 @@
-/* Crea un programa que calcule el precio final de un producto después de aplicarle un descuento.
-
-Solicita al usuario que inserte el precio original del producto.
-Solicita al usuario que inserte el porcentaje de descuento a aplicar (por ejemplo, 10 para un descuento del 10%).
-Utiliza una función para calcular el precio final después de aplicar el descuento.
-Muestra el precio final al usuario. */
